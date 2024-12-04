9f362aeddf3de54b6075b849df9be6435fea303a
Charles Heckroth <checkroth@gmail.com>
2024-12-04 13:01:36 +0900

    Refactoring to prepare for day 2 - allowing shared input processing

--- aoc_2024/src/main.rs
+++ aoc_2024/src/main.rs
@@ -2,5 +2,9 @@ mod week1;
 
 fn main() {
     println!("Day 1");
-    week1::day1()
+    week1::day1();
+    println!("\nDay 2");
+    week1::day2();
+
+    ()
 }
--- aoc_2024/src/week1.rs
+++ aoc_2024/src/week1.rs
@@ -1,22 +1,32 @@
 use std::{collections::HashMap, fs};
 
+
 fn insert_str_as_int_in_order(ordered_vec: &mut Vec<i32>, new_item: &str) {
     let parsed = new_item.parse::<i32>().unwrap();
     let position = ordered_vec.binary_search(&parsed).unwrap_or_else(|err| err);
     ordered_vec.insert(position, parsed);
 }
 
+/// Reads the input file as a vector of strings.
+/// Utility for all aoc problems, as they all begin with a huge text file.
+/// All further processing and formatting is highly dependent on the problem, so it ends there.
+/// Would technically be more efficient to handle a Vec<&str> instead of converting to String objects,
+///     but for re-usability it's a necessary step.
+fn read_input_as_lines(input_filename: &str) -> Vec<String> {
+    let contents = fs::read_to_string(format!("input/{}", input_filename)).expect("input not found");
+    contents.split("\n").map(String::from).collect()
+}
+
 pub fn day1() {
-    let contents = fs::read_to_string("input/day1_input.txt").expect("input not found");
-    let lines: Vec<&str> = contents.split("\n").collect();
+    let lines = read_input_as_lines("day1_input.txt");
     // Mutable vectors for ease of building from parsed input
     let mut left_col: Vec<i32> = Vec::new();
     let mut right_col: Vec<i32> = Vec::new();
 
     for line in lines.iter() {
         line.split_once("   ").map(|(l, r)| {
-            insert_str_as_int_in_order(&mut left_col, &l);
-            insert_str_as_int_in_order(&mut right_col, &r);
+            insert_str_as_int_in_order(&mut left_col, l);
+            insert_str_as_int_in_order(&mut right_col, r);
         });
     }
 
@@ -42,4 +52,11 @@ pub fn day1() {
     println!("Part 1 (Distances): {}", distances_sum);
     let similarity_sum: i32 = similarity.sum();
     println!("Part 2 (Similarity Score): {}", similarity_sum);
+
+    assert_eq!(distances_sum, 1722302);
+    assert_eq!(similarity_sum, 20373490);
+}
+
+pub fn day2() {
+    println!("Day 2 WIP")
 }
